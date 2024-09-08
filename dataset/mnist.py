fbc89a5b21fdbe9643dc7df7d5d3b2ef8624b437
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-09-08 18:26:51 +0900

    add : I added and formatted the code of chapter five.

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -2,7 +2,7 @@
 try:
     import urllib.request
 except ImportError:
-    raise ImportError('You should use Python 3.x')
+    raise ImportError("You should use Python 3.x")
 import os.path
 import gzip
 import pickle
@@ -10,14 +10,14 @@ import os
 import numpy as np
 
 
-#url_base = 'http://yann.lecun.com/exdb/mnist/'
-url_base = 'https://ossci-datasets.s3.amazonaws.com/mnist/'  # mirror site
+# url_base = 'http://yann.lecun.com/exdb/mnist/'
+url_base = "https://ossci-datasets.s3.amazonaws.com/mnist/"  # mirror site
 
 key_file = {
-    'train_img':'train-images-idx3-ubyte.gz',
-    'train_label':'train-labels-idx1-ubyte.gz',
-    'test_img':'t10k-images-idx3-ubyte.gz',
-    'test_label':'t10k-labels-idx1-ubyte.gz'
+    "train_img": "train-images-idx3-ubyte.gz",
+    "train_label": "train-labels-idx1-ubyte.gz",
+    "test_img": "t10k-images-idx3-ubyte.gz",
+    "test_label": "t10k-labels-idx1-ubyte.gz",
 }
 
 dataset_dir = os.path.dirname(os.path.abspath(__file__))
@@ -36,54 +36,62 @@ def _download(file_name):
         return
 
     print("Downloading " + file_name + " ... ")
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"}
-    request = urllib.request.Request(url_base+file_name, headers=headers)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
+    }
+    request = urllib.request.Request(url_base + file_name, headers=headers)
     response = urllib.request.urlopen(request).read()
-    with open(file_path, mode='wb') as f:
+    with open(file_path, mode="wb") as f:
         f.write(response)
     print("Done")
 
+
 def download_mnist():
     for v in key_file.values():
-       _download(v)
+        _download(v)
+
 
 def _load_label(file_name):
     file_path = dataset_dir + "/" + file_name
 
     print("Converting " + file_name + " to NumPy Array ...")
-    with gzip.open(file_path, 'rb') as f:
-            labels = np.frombuffer(f.read(), np.uint8, offset=8)
+    with gzip.open(file_path, "rb") as f:
+        labels = np.frombuffer(f.read(), np.uint8, offset=8)
     print("Done")
 
     return labels
+
 
 def _load_img(file_name):
     file_path = dataset_dir + "/" + file_name
 
     print("Converting " + file_name + " to NumPy Array ...")
-    with gzip.open(file_path, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=16)
+    with gzip.open(file_path, "rb") as f:
+        data = np.frombuffer(f.read(), np.uint8, offset=16)
     data = data.reshape(-1, img_size)
     print("Done")
 
     return data
 
+
 def _convert_numpy():
     dataset = {}
-    dataset['train_img'] =  _load_img(key_file['train_img'])
-    dataset['train_label'] = _load_label(key_file['train_label'])
-    dataset['test_img'] = _load_img(key_file['test_img'])
-    dataset['test_label'] = _load_label(key_file['test_label'])
+    dataset["train_img"] = _load_img(key_file["train_img"])
+    dataset["train_label"] = _load_label(key_file["train_label"])
+    dataset["test_img"] = _load_img(key_file["test_img"])
+    dataset["test_label"] = _load_label(key_file["test_label"])
 
     return dataset
+
 
 def init_mnist():
     download_mnist()
     dataset = _convert_numpy()
     print("Creating pickle file ...")
-    with open(save_file, 'wb') as f:
+    with open(save_file, "wb") as f:
         pickle.dump(dataset, f, -1)
     print("Done!")
+
 
 def _change_one_hot_label(X):
     T = np.zeros((X.size, 10))
@@ -111,24 +119,27 @@ def load_mnist(normalize=True, flatten=True, one_hot_label=False):
     if not os.path.exists(save_file):
         init_mnist()
 
-    with open(save_file, 'rb') as f:
+    with open(save_file, "rb") as f:
         dataset = pickle.load(f)
 
     if normalize:
-        for key in ('train_img', 'test_img'):
+        for key in ("train_img", "test_img"):
             dataset[key] = dataset[key].astype(np.float32)
             dataset[key] /= 255.0
 
     if one_hot_label:
-        dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
-        dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
+        dataset["train_label"] = _change_one_hot_label(dataset["train_label"])
+        dataset["test_label"] = _change_one_hot_label(dataset["test_label"])
 
     if not flatten:
-         for key in ('train_img', 'test_img'):
+        for key in ("train_img", "test_img"):
             dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
 
-    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
+    return (dataset["train_img"], dataset["train_label"]), (
+        dataset["test_img"],
+        dataset["test_label"],
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     init_mnist()
